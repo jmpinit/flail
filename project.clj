40b66823ca46b6fe0ea4949e440fc995952fa6ff
Owen Trueblood <hi@owentrueblood.com>
2019-03-18 11:19:52 -0400

    Create LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject flail "0.1.0-SNAPSHOT"
   :description "Creative coding environment for KUKA industrial robot arms"
   :url "https://github.com/haploid/flail"
-  :license {:name "GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.gnu.org/software/classpath/license.html"}
+  :license {:name "GPL-3.0-or-later"
+            :url "https://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot flail.core
   :target-path "target/%s"
